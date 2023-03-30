310a5355331d80a01f5d1b9db2da5d77cf94dcac
gary <garyhe.05@outlook.com>
2023-03-30 23:01:31 +0800

    fix: change model

--- src/routes/api/explain/+server.ts
+++ src/routes/api/explain/+server.ts
@@ -35,7 +35,7 @@ export const POST: RequestHandler = async ({ request }) => {
         `;
 
 		const completionOpts: CreateCompletionRequest = {
-			model: 'gpt-3.5-turbo',
+			model: 'text-davinci-003',
 			prompt: generatedPrompt,
 			max_tokens: 1000,
 			temperature: 0.9,
